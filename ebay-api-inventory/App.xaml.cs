73250b64c4f5f04c1cf799ab4d62432270e6788d
francisco <franke1615@gmail.com>
2023-01-27 18:16:06 -0800

    saving work on login screen

--- ebay-api-inventory/App.xaml.cs
+++ ebay-api-inventory/App.xaml.cs
@@ -6,12 +6,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace ebay_api_inventory
+namespace ebay_api_inventory;
+
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
 }
--- ebay-api-inventory/MainWindow.xaml.cs
+++ ebay-api-inventory/MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace ebay_api_inventory;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow()
